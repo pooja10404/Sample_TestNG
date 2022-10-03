49545dcf724148568ed0d67a318575a697302a2a
Arti <artitongia.es@gmail.com>
2022-10-03 17:58:12 +0530

    added new test case for alerts get text functionality

--- src/alertEnterTextFunctionality.java
+++ src/alertEnterTextFunctionality.java
@@ -1,6 +1,10 @@
-import org.openqa.selenium.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
@@ -8,7 +12,7 @@ import org.testng.annotations.Test;
 import java.time.Duration;
 import java.util.function.Function;
 
-public class alertDismissFunctionality {
+public class alertGetTextFunctionality {
 
     WebDriver driver;
     //Method to set up the browser and open the website
@@ -23,12 +27,13 @@ public class alertDismissFunctionality {
     public void alert_Functionality() {
        try {
             System.out.println("Starting the test case");
-            WebElement clickMeButton = driver.findElement(By.id("confirmButton"));
+            WebElement clickMeButton = driver.findElement(By.id("alertButton"));
             clickMeButton.click();
             driver.switchTo().alert();
-            driver.switchTo().alert().dismiss();
-//            driver.switchTo().defaultContent();
-            }catch(UnhandledAlertException e){e.getAlert();}
+            String msg = driver.switchTo().alert().getText();
+            System.out.println("msg===="+msg);
+            Assert.assertEquals("You clicked a button", msg);
+            }catch(Exception e){}
         }
 
 
