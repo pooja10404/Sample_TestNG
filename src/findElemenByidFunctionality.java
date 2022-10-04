25f4ed307f55a521b9161248746150ac6edb9a65
Arti <artitongia.es@gmail.com>
2022-10-04 15:50:14 +0530

    add test case

---
+++ src/findElemenByidFunctionality.java
@@ -0,0 +1,45 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+import java.util.function.Function;
+
+public class findelemenbyidfunctionality {
+
+    WebDriver driver;
+    //Method to set up the browser and open the website
+    @BeforeClass
+    public void setUp() {
+        System.setProperty("webdriver.chrome.driver", "C://Users//Pooja//jar files//chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.get("http://demo.guru99.com/test/ajax.html");
+        driver.manage().window().maximize();
+    }
+    @Test(priority = 1)
+    public void findelement_By_id_Functionality() {
+        try {
+
+            //Get the Web Element corresponding to the field Email (Text field) and use sendkeys to pass the email to the field
+            System.out.println("Starting the test case");
+            // Find the radio button for “No” using its ID and click on it
+            driver.findElement(By.id("no")).click();
+            //Click on Check Button
+            driver.findElement(By.id("buttoncheck")).click();
+        }catch(Exception e){}
+    }
+
+    @AfterClass
+    public void tearDown() {
+        driver.close();
+        driver.quit();
+        driver=null;
+    }
+
+}
