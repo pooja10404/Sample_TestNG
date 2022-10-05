87889beccf32e843fa71a6cb3dc24cfdb1888d81
nishant <nishantsinha.es@gmail.com>
2022-10-05 14:08:41 +0530

    updated all the files

--- src/alertDismissFunctionality.java
+++ src/alertDismissFunctionality.java
@@ -5,6 +5,7 @@ import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
+import java.io.File;
 import java.time.Duration;
 import java.util.function.Function;
 
@@ -14,7 +15,12 @@ public class alertDismissFunctionality {
     //Method to set up the browser and open the website
     @BeforeClass
     public void setUp() {
-        System.setProperty("webdriver.chrome.driver", "C://Users//Pooja//Demo_TestNG_Project//tools//chromedriver.exe");
+        File f = new File("tools");
+
+        // Get the absolute path of file f
+        String absolute = f.getAbsolutePath();
+        System.out.println("absolute=="+absolute);
+        System.setProperty("webdriver.chrome.driver", absolute+"//chromedriver.exe");
         driver = new ChromeDriver();
         driver.get("https://demoqa.com/alerts");
         driver.manage().window().maximize();
