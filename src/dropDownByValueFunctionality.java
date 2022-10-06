54805b1f6e4fa34109aeec4d1e713766e3d66724
shivam <shivamdubey.es@gmail.com>
2022-10-06 17:42:46 +0530

    added dropdown test case

---
+++ src/dropDownByValueFunctionality.java
@@ -0,0 +1,38 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import java.io.File;
+
+public class dropDownByValueFunctionality {
+    WebDriver driver;
+
+    //Method to set up the browser and open the website
+    @BeforeTest
+    public void setUp() {
+        File f = new File("tools");
+        // Get the absolute path of file f
+        String absolute = f.getAbsolutePath();
+        System.setProperty("webdriver.chrome.driver", absolute + "//chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.get("https://demo.guru99.com/test/newtours/register.php");
+        driver.manage().window().maximize();
+
+    }
+    @Test(priority = 1)
+    public void dropDownByValueFunctionality() throws InterruptedException {
+        Select dropdown = new Select(driver.findElement(By.name("country")));// this is select class
+        // select dropdown by value
+        dropdown.selectByValue("ANDORRA");
+    }
+    @AfterClass
+    public void tearDown() {
+        driver.close();
+        driver.quit();
+        driver=null;
+    }
+}
